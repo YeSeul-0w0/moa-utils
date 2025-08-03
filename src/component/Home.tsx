45a62c1e3fd32aff097190511d092af05c0ffb27
YeSeul-0w0 <choi333ys@gmail.com>
2025-08-03 14:57:33 +0900

    chore: code cleanup

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -8,10 +8,7 @@ const Home = () => {
 		<main className="flex flex-col items-center justify-between px-4 py-20 text-center">
 			<div className="text-xs text-gray-600">일상 생활에서 유용한 기능들을</div>
 			<h1 className="text-2xl font-bold mt-1 mb-6">MoA</h1>
-
-			<section className="bg-gray-100 rounded-xl my-10 p-6 flex gap-10 justify-center w-full max-w-sm">
-				<div className="flex flex-col items-center cursor-pointer"></div>
-			</section>
+			<section className="bg-gray-100 rounded-xl my-10 p-6 flex gap-10 justify-center w-full max-w-sm"></section>
 		</main>
 	);
 };
