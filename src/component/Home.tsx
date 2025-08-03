17024727ca8c34c38feeabdb016aeeb99c70bda7
YeSeul-0w0 <choi333ys@gmail.com>
2025-08-03 16:02:57 +0900

    chore: delete unnecessary code

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { Calendar, MapPin } from "lucide-react";
+import { Calendar } from "lucide-react";
 
 const Home = () => {
 	const navigate = useNavigate();
@@ -8,7 +8,15 @@ const Home = () => {
 		<main className="flex flex-col items-center justify-between px-4 py-20 text-center">
 			<div className="text-xs text-gray-600">일상 생활에서 유용한 기능들을</div>
 			<h1 className="text-2xl font-bold mt-1 mb-6">MoA</h1>
-			<section className="bg-gray-100 rounded-xl my-10 p-6 flex gap-10 justify-center w-full max-w-sm"></section>
+			<section className="bg-gray-100 rounded-xl my-10 p-6 flex gap-10 justify-center w-full max-w-sm">
+				<div
+					className="flex flex-col items-center cursor-pointer"
+					onClick={() => navigate("/dday")}
+				>
+					<Calendar size={28} />
+					<span className="mt-2 font-semibold text-sm">디데이</span>
+				</div>
+			</section>
 		</main>
 	);
 };
